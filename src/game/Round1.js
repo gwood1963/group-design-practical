20d2d65c1ab2acb4624da67e5ab87aec9cdcadfe
ThematicMountain1 <khanacademyuser66@gmail.com>
2023-03-21 19:30:34 +0000

    edited file round1.js

--- src/game/Round1.js
+++ src/game/Round1.js
@@ -1,3 +1,13 @@
 /*
 This file serves to represent the backend needed for roudn 1
 */
+
+class Round1 {
+    theGraph = new Graph;
+
+    constructor() {}
+
+    loadGraph(graph) {
+        this.theGraph = graph;
+    }
+}
