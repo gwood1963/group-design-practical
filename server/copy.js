9d55f5698fd5dac29768ad198c9e5701e80111f3
gwood1963 <gwoodcode@gmail.com>
2023-04-11 09:45:17 +0100

    fixes to azure deployment

---
+++ server/copy.js
@@ -0,0 +1,25 @@
+// This is a simple script to copy the server folder to the "build" directory
+const { promises: fs } = require("fs");
+const path = require("path");
+
+async function copyDir(src, dest) {
+  await fs.mkdir(dest, { recursive: true });
+  let entries = await fs.readdir(src, { withFileTypes: true });
+
+  for (let entry of entries) {
+    let srcPath = path.join(src, entry.name);
+    let destPath = path.join(dest, entry.name);
+
+    entry.isDirectory()
+      ? await copyDir(srcPath, destPath)
+      : await fs.copyFile(srcPath, destPath);
+  }
+}
+
+async function main() {
+  await copyDir("build", "dist/build");
+  await copyDir("public", "dist/public");
+  await copyDir("views", "dist/views");
+}
+
+main();
